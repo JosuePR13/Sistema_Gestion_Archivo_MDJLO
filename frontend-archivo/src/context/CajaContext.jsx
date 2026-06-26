1963c3483d1370c61938c46bfaa6bfe6405ad35f
JosuePR13 <josue.13.02.04@gmail.com>
2026-06-26 12:31:23 -0500

    feat: modulos caja y reporte - Parte 1

--- frontend-archivo/src/context/CajaContext.jsx
+++ frontend-archivo/src/context/CajaContext.jsx
@@ -1,9 +1,7 @@
 import { createContext, useContext } from 'react';
 
-// 1. Creamos el contexto raíz con un valor inicial nulo
 export const CajaContext = createContext(null);
 
-// 2. Hook personalizado para consumir la data de forma directa en el Front
 export function useCaja() {
     const context = useContext(CajaContext);
     if (!context) {
